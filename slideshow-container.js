c179c9f77c1e7db54cd0aca7ff79ea36a2019691
Gauthier Östervall <gauthier@cipherstone.com>
2018-03-15 15:38:09 +0100

    Initial commit

---
+++ slideshow-container.js
@@ -0,0 +1,45 @@
+var slideIndex = 0;
+carousel();
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex >= x.length) {slideIndex = 0}
+    x[slideIndex].style.display = "block";
+    //document.getElementById("debug").innerHTML = x[slideIndex-1].dataset.timeout;
+    timeout = x[slideIndex].dataset.timeout;
+    if (!timeout) {
+        timeout = 500; // Default
+    }
+    setTimeout(carousel, timeout); // Get display time from html instead!
+}
